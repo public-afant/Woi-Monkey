37e5b8267a0aae7e6960f12a911f0ec62102f816
AFANT <public.afant@gmail.com>
2024-10-14 23:46:57 +0900

    Splash_Screen View 임시 작성

---
+++ Woi-Monkey/Splash_Screen.swift
@@ -0,0 +1,18 @@
+//
+//  Splash_Screen.swift
+//  Woi-Monkey
+//
+//  Created by YH on 10/14/24.
+//
+
+import SwiftUI
+
+struct Splash_Screen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Splash_Screen()
+}
