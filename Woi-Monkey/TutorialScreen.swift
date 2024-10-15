391911e4c59104df3443b806726d4362864129a8
AFANT <public.afant@gmail.com>
2024-10-15 14:45:09 +0900

    Update

---
+++ Woi-Monkey/TutorialScreen.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialScreen.swift
+//  Woi-Monkey
+//
+//  Created by YH on 10/15/24.
+//
+
+import SwiftUI
+
+struct TutorialScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TutorialScreen()
+}
