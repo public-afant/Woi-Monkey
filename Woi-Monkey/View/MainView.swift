380220631804071ac20c5ae1eefb2659c79d4ad8
AFANT <public.afant@gmail.com>
2024-10-18 21:54:15 +0900

    Design: MainView 배경 색상 변경

--- Woi-Monkey/View/MainView.swift
+++ Woi-Monkey/View/MainView.swift
@@ -10,32 +10,38 @@ import SwiftUI
 struct MainView: View {
     
     @State  var path: [String] = []
-    
+   
     var body: some View {
         NavigationStack(path:$path){
-            VStack{
-                Text("Main Page(Monkey time)").foregroundColor(.bone)
-                Spacer()
-                
-                //커스텀 컴포넌트 버튼
-                ButtonView(title: "Next", backgroundColor: .primaryColor, size: 18, action: {path.append("TodoView")})
-                
-            }.navigationDestination(for: String.self){
-                value in
-                switch value{
-                case "TodoView":
-                    TodoView(path: $path).navigationBarBackButtonHidden(true)
-                case "SetTimerView":
-                    SetTimerView(path:$path).navigationBarBackButtonHidden(true)
-                case "TimerView":
-                    TimerView(path:$path).navigationTitle("TimerView").navigationBarBackButtonHidden(true)
-                case "DashboardView":
-                    DashboardView(path:$path).navigationTitle("DashboardView").navigationBarBackButtonHidden(true)
-                default:
-                    Text("Error!")
-                }
-                
-            }.navigationTitle("MainView").padding()
+            
+            ZStack{
+                Color.primaryWhite.ignoresSafeArea()
+                VStack{
+                    Text("Main Page\n(Monkey time)").foregroundColor(.orange).font(.customFont32).multilineTextAlignment(.center)
+                    Spacer()
+                    
+                    
+                    
+                    //커스텀 컴포넌트 버튼
+                    ButtonView(title: "다 음", backgroundColor: .primaryColor, size: 18, action: {path.append("TodoView")})
+                    
+                }.navigationDestination(for: String.self){
+                    value in
+                    switch value{
+                    case "TodoView":
+                        TodoView(path: $path).navigationBarBackButtonHidden(true)
+                    case "SetTimerView":
+                        SetTimerView(path:$path).navigationBarBackButtonHidden(true)
+                    case "TimerView":
+                        TimerView(path:$path).navigationTitle("TimerView").navigationBarBackButtonHidden(true)
+                    case "DashboardView":
+                        DashboardView(path:$path).navigationTitle("DashboardView").navigationBarBackButtonHidden(true)
+                    default:
+                        Text("Error!")
+                    }
+                    
+                }.padding()
+            }
         }
     }
 }
