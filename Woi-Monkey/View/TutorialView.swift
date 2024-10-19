374efd7ebea53d21dfe30d82934f9ce5173b23f7
AFANT <public.afant@gmail.com>
2024-10-19 21:12:48 +0900

    Test: TutorialView 이미지 테스트

--- Woi-Monkey/View/TutorialView.swift
+++ Woi-Monkey/View/TutorialView.swift
@@ -11,30 +11,34 @@ import SwiftUI
 struct TutorialView: View {
     
     @State  var path: [String] = []
-    
+    var text = "우리의 뇌에는 때때로 즉각적인 만족을 추구하는 원숭이가 놀러옵니다."
     
     var body: some View {
         
         
-        VStack{
+        ZStack{
+            Color.primaryWhite.ignoresSafeArea()
+            VStack{
+                Spacer()
+                Image("monkeyPixel").resizable().frame(width: 320, height: 300)
+                ZStack(alignment: .topLeading){
+                    Image("text_box").resizable().frame(width: .infinity, height: 150)
+                    VStack{
+                        
+                        Text(text.split(separator: "").joined(separator: "\u{200B}"))
+                            .multilineTextAlignment(.leading).font(.customFont18).lineSpacing(8)
+                    }.padding(40.0)
+                }
+                
+                Spacer()
+                //커스텀 컴포넌트 버튼
+                ButtonView(title: "Next", backgroundColor: .primaryColor, size: 18, action: {path.append("MainView")})
+                
+                
+            }.padding()
             
-            Text("Tutorial View")
-            Text("Tutorial View")
-            Text("Tutorial View")
-            Text("Tutorial View")
-            Text("Tutorial View")
-            
-            Spacer()
-            
-            
-            //커스텀 컴포넌트 버튼
-            ButtonView(title: "Next", backgroundColor: .primaryColor, size: 18, action: {path.append("MainView")})
-            
-            
-        }.padding()
-        
+        }
     }
-    
 }
 
 
