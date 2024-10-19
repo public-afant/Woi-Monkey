e68d15637e87fa6231185293a0b8b35f61828c05
AFANT <public.afant@gmail.com>
2024-10-19 21:14:14 +0900

    Design: MainView 임시 페이지, Todo를 위한 title  변수 선언 및 Binding

--- Woi-Monkey/View/MainView.swift
+++ Woi-Monkey/View/MainView.swift
@@ -6,43 +6,65 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct MainView: View {
     
-    @State  var path: [String] = []
-   
+    @State var path: [String] = []
+    @State var title: String = ""
+    @State var isBlinking: Bool = false
+    
     var body: some View {
         NavigationStack(path:$path){
             
-            ZStack{
-                Color.primaryWhite.ignoresSafeArea()
-                VStack{
-                    Spacer()
-                    Text("Main Page\n(Monkey time)").foregroundColor(.orange).font(.customFont32).multilineTextAlignment(.center)
-                    Spacer()
-                    
-                    
-                    
-                    //커스텀 컴포넌트 버튼
-                    ButtonView(title: "다 음", backgroundColor: .primaryColor, size: 18, action: {path.append("TodoView")})
-                    
-                }.navigationDestination(for: String.self){
-                    value in
-                    switch value{
-                    case "TodoView":
-                        TodoView(path: $path).navigationBarBackButtonHidden(true)
-                    case "SetTimerView":
-                        SetTimerView(path:$path).navigationBarBackButtonHidden(true)
-                    case "TimerView":
-                        TimerView(path:$path).navigationTitle("TimerView").navigationBarBackButtonHidden(true)
-                    case "DashboardView":
-                        DashboardView(path:$path).navigationTitle("DashboardView").navigationBarBackButtonHidden(true)
-                    default:
-                        Text("Error!")
-                    }
-                    
-                }.padding()
-            }
+            GeometryReader { geometry in
+                ZStack{
+                    Color.primaryChampangne.ignoresSafeArea()
+                    VStack{
+                        Spacer()
+                        
+//                        Text("Main Page\n(Monkey time)").foregroundColor(.orange).font(.customFont32).multilineTextAlignment(.center)
+                        AnimatedImage(name:"monkey_ori.gif").frame(width: 300, height: 300)
+                        Text("Touch to Start").font(.customFont36).padding()
+                            .opacity(isBlinking ? 0.2 : 1)  // 투명도를 애니메이션으로 변경
+                            .foregroundStyle(.primaryOrange)
+                            .animation(Animation.easeInOut(duration: 0.7).repeatForever(autoreverses: true), value: isBlinking)
+                            .onAppear {
+                                isBlinking = true
+                            }
+                        
+                        
+                        Spacer()
+                        
+                        
+                        Image("monkeyPixel") // 이미지 파일 이름을 입력
+                            .resizable() // 이미지 크기를 조정할 수 있게 함
+                            .scaledToFill() // 이미지를 화면에 꽉 채우기 위해 사용
+                            .frame(width: geometry.size.width-100, height: geometry.size.height*0.2) // 화면 하단 30%를 이미지로 채움
+//                            .clipped() // 이미지가 넘치는 부분을 잘라냄
+                        
+                        
+                        
+                    }.navigationDestination(for: String.self){
+                        value in
+                        switch value{
+                        case "TodoView":
+                            TodoView(path: $path, title: $title).navigationBarBackButtonHidden(false)
+                        case "SetTimerView":
+                            SetTimerView(path: $path).navigationBarBackButtonHidden(false)
+                        case "TimerView":
+                            TimerView(path: $path).navigationTitle("TimerView").navigationBarBackButtonHidden(false)
+                        case "DashboardView":
+                            DashboardView(path: $path, title: $title).navigationBarBackButtonHidden(true)
+                        default:
+                            Text("Error!")
+                        }
+                        
+                    }.padding()
+                }.onTapGesture {
+                    path.append("TodoView")
+                }
+            }.edgesIgnoringSafeArea(.bottom)
         }
     }
 }
