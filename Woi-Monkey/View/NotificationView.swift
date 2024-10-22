6d37f49afc2d9f60d56c656ca8a279425e6241ea
hyunjin <coolhyunjin@gmail.com>
2024-10-22 22:15:58 +0900

    Design: 다른 뷰와 디자인 유사하도록 수정 & 알림 시간 기준은 현재 시간으로 설정

--- Woi-Monkey/View/NotificationView.swift
+++ Woi-Monkey/View/NotificationView.swift
@@ -4,77 +4,90 @@ struct NotificationView: View {
     @Binding var path: [String]
     @Binding var title: String
     
-    @State private var selectedHour: Int = 11  // 기본 시간: 오전 9시
-    @State private var selectedMinute: Int = 52 // 기본 분: 00분
+    @State private var selectedHour: Int = 0
+    @State private var selectedMinute: Int = 0
     
-
     var body: some View {
-        VStack(spacing: 20) {
+        ZStack {
             Color.primaryWhite.ignoresSafeArea()
-            Text("알림 설정")
-                .font(.customFontBig)
-                .padding()
-            
-            // 시간 선택기
-            HStack {
-                Picker("Hour", selection: $selectedHour) {
-                    ForEach(0..<24) { hour in
-                        Text("\(hour)시").tag(hour).font(.customFont26)
+            VStack {
+                VStack {
+                    Text("다음 집중시간 알림을 설정해주세요")
+                        .font(.customFont26)
+                    
+                    HStack {
+                        Picker("Hour", selection: $selectedHour) {
+                            ForEach(0..<24) { hour in
+                                Text("\(hour)시").tag(hour).font(.customFont18)
+                            }
+                        }
+                        .pickerStyle(WheelPickerStyle())
+                        Picker("Minute", selection: $selectedMinute) {
+                            ForEach(0..<60) { minute in
+                                Text("\(minute)분").tag(minute).font(.customFont18)
+                            }
+                        }
+                        .pickerStyle(WheelPickerStyle())
                     }
-                }
-                .pickerStyle(WheelPickerStyle())
-                
-                Picker("Minute", selection: $selectedMinute) {
-                    ForEach(0..<60) { minute in
-                        Text("\(minute)분").tag(minute).font(.customFont26)
+                    .frame(height: 150)
+                    Button(action: {
+                        path.removeAll()
+                    }) {
+                        Text("설 정")
+                            .font(.customFont26)
+                            .frame(width: 200, height: 50)
+                            .background(Color.primaryColor)
+                            .foregroundColor(.black)
+                            .cornerRadius(10)
                     }
+                    //                    ButtonView(
+                    //                        title: "설 정",
+                    //                        backgroundColor: .primaryColor,
+                    //                        size: 25,
+                    //                        action: {path.append("TimerView")}
+                    //                    )
                 }
-                .pickerStyle(WheelPickerStyle())
+                .padding(.horizontal, 30.0)
+                .padding(.top, 40.0)
+                .padding(.bottom, 30.0)
+                .background(Color.primaryChampangne)
+                .cornerRadius(15)
             }
-            Spacer()
-            .frame(height: 150)
-            
-            
-            // 선택한 시간에 알림 예약 버튼
-            Button("알림 예약") {
-                scheduleNotification()
+            .padding(.horizontal, 20.0)
+        }.navigationBarItems(leading: BackButton(path: $path))
+            .onAppear {
+                // Date.now를 사용하여 현재 시간 설정
+                let currentDate = Date.now
+                let calendar = Calendar.current
+                selectedHour = calendar.component(.hour, from: currentDate)
+                selectedMinute = calendar.component(.minute, from: currentDate)
                 
+                NotificationManager.shared.requestNotificationPermission()
             }
-            .font(.customFont26)
-            .padding()
-            .background(Color.primaryChampangne)
-            .foregroundColor(Color.primaryWhite)
-            .cornerRadius(10)
-        }
-        Spacer()
-        
-        .onAppear {
-            NotificationManager.shared.requestNotificationPermission()
-        }
-    }
-    
-    
-    func scheduleNotification() {
-        let content = UNMutableNotificationContent()
-        content.title = "워이~ 몽키"
-        content.body = "머릿속의 몽키를 내쫒아주세요"
-        content.sound = .default
-
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-
-        UNUserNotificationCenter.current().add(request) { error in
-            if let error = error {
-                print("알림 추가 실패: \(error.localizedDescription)")
-            } else {
-                // 알림이 성공적으로 추가된 후 앱 처음으로 돌아가기
-                DispatchQueue.main.async {
-                    path.removeAll()  // 변경 가능한 Binding으로 선언된 path 수정
-                }
-            }
-        }
     }
 }
+//    
+//    
+//    func scheduleNotification() {
+//        let content = UNMutableNotificationContent()
+//        content.title = "워이~ 몽키"
+//        content.body = "머릿속의 몽키를 내쫒아주세요"
+//        content.sound = .default
+//
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+//        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+//
+//        UNUserNotificationCenter.current().add(request) { error in
+//            if let error = error {
+//                print("알림 추가 실패: \(error.localizedDescription)")
+//            } else {
+//                // 알림이 성공적으로 추가된 후 앱 처음으로 돌아가기
+//                DispatchQueue.main.async {
+//                    path.removeAll()  // 변경 가능한 Binding으로 선언된 path 수정
+//                }
+//            }
+//        }
+//    }
 
 #Preview {
     NotificationView(path: .constant([]), title: .constant(""))
